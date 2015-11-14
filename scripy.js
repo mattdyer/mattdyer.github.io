0900c1be61fe175db66485836da01cb4703773de
mattdyer <madmatt1220@gmail.com>
2015-11-14 09:57:40 -0700

    working on scripy

---
+++ scripy.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function(){
+   var memory = [10,20,30];
+   
+   //requestAnimationFrame(function(){
+   setTimeout(function(){
+      
+      console.log(memory);
+   },1000);
+   //});
+   
+})();
